4b9fd88431552377e757ce671b04ca0cc932a425
Eduardo Almeida <eduardoalmeida258@gmail.com>
2017-10-20 19:49:20 -0300

    Merge

--- vetores.c
+++ vetores.c
@@ -4,27 +4,38 @@ void main(void){
 
     int notas[10];
     int posicao[10];
-    int i,temp,j = 0;
+    int i,temp = 0;
     int encontrado = 0;
+
+    // Recebi os dez numeros e coloquei no VETOR
     for(i = 0; i <= 10; i++){
         printf("Digite o valor para posicao: %d\n", i);
         scanf("%d",&notas[i]);
     }
 
+    // Recebo um numero
     printf("Digite um valor para pesquisar: ");
     scanf("%d",&temp);
+    
 
+    // Pesquiso dentro do Vetor
     for(i = 0; i <= 10; i++){
+
         if( temp == notas[i]){
+
+            // Encontrado pelo menos uma vez...
             encontrado = 1;
             posicao[i] = i;
         }
     }
+
+    
+    // Se encontrado pelo menos uma vez, eu vou exibir "Encontrado na posição: [posicoes]"
     if(encontrado == 1){
-        printf("Encontrado na posicao: ");
+        // printf("\nEncontrado na posicao: ");
         for(i = 0; i <= 10; i++){
-            if(temp == [i]){
-                printf("%d ",i);
+            if(temp == posicao[i]){
+                printf("%d ",posicao[i]);
             }
         }
     }
