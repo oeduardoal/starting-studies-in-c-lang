bc0d4338b77d4ee647428fa5f70bcd28107c8f41
Eduardo Almeida <eduardoalmeida258@gmail.com>
2017-10-21 14:50:06 -0300

    05 - Problem

--- L3_05.c
+++ L3_05.c
@@ -9,44 +9,24 @@
 
 int main()
 {
-    int K[5],N[5];
-    int c,i,atual,prox,ante = 0;
+    int K[TM],N[TM];
+    int c,valor = 0;
     
     // zerar
-    for(c = 0 ; c < 5; c++){
+    for(c = 0 ; c < TM; c++){
         K[c]   = 0;
-        N[c]   = 0;
     }
          
     for(c = 0 ; c < TM; c++){
-        printf("Escreva o numero para: %d (Primeiro Vetor) ...... ",c);
+        printf("Escreva o numero para: %d ( VETOR | Produto 1 ) ...... ", c);
         scanf("%d",&K[c]);
     }
 
+    printf("Escreva o valor para ser multiplicado ( Produto 2 ) ...... ");
+    scanf("%d",&valor);
+
     for(c = 0 ; c < TM ; c ++){
-        // São ímpares
-        N[c] = K[c];
-        if(K[c] % 2 == 1){
-            for( i = c; i < TM ; i++){
-                if(K[i] % 2 == 0){
-                    
-                    atual = K[c];
-                    
-                    K[c] = K[i];
-                    
-                    K[i] = atual;
-
-                    i = 5;
-                }
-            }
-        }
-    }
-
-    printf("\n\n");
-
-    printf("\tNovo array:\n\n");
-    for(c = 0 ; c < TM ; c ++){
-        printf("\t[%d] = %d\n",c, K[c]);
+        printf("\n %d x %d = %d", K[c], valor, K[c]*valor);
     }
 
     printf("\n\n");
