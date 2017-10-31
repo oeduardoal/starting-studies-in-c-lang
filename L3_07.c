009f9e24991cae319a4a21bab08712e936f1fea9
Eduardo Almeida <oeduardoal@gmail.com>
2017-10-31 01:00:42 -0300

    07

---
+++ L3_07.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define TM 3
+int main(){
+
+    int vetorA[TM],vetorB[TM],vetorR[TM] = {0};
+    int c;
+
+
+    printf("Entre com os valores do vetor A: \n");
+    for(c = 0; c < TM; c++)
+        scanf("%d",&vetorA[c]);
+
+    printf("\n\n");
+
+    printf("Entre com os valores do vetor B: \n");        
+    for(c = 0; c < TM; c++)
+        scanf("%d", &vetorB[c]);    
+
+    for(c = 0; c < TM; c++){
+        vetorR[c] = vetorA[c] + vetorB[c];
+    }
+
+    printf("\n\n");
+
+    for(c = 0; c < TM; c++){
+        printf("%d + %d = %d\n",vetorA[c],vetorB[c],vetorR[c]);
+    }
+
+    return 0;
+}
