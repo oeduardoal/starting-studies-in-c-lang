d162a2faa5d007f1bf7ad35c573867599504dff9
eduardoalmeida258 <eduardoalmeida258@gmail.com>
2018-03-07 19:54:26 -0500

    EAO00002

--- EAO0002.c
+++ EAO0002.c
@@ -5,9 +5,10 @@
 int main(){
 	
     int i[10] = {10,9,8,7,6,5,4,3,2,1};
-	int c;
-    for(c = 10; c <= 0;c--){
-		printf("\n%d",c);
+
+	
+    for(int c = sizeof(i)/4 - 1; c >= 0;c--){
+		printf("\n%d",i[c]);
 	}
         
     
