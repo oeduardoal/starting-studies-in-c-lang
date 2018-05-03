c92b8661cfb9eb7130c297114e69a4c76d785ecf
Eduardo Almeida <eduardoalmeida258@gmail.com>
2018-05-02 21:28:01 -0300

    Update OrdernarSelecao.c

--- matriz/OrdernarSelecao.c
+++ matriz/OrdernarSelecao.c
@@ -1,1 +1,66 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#define TM 5
+/*
+* @ Descricao: Ordenacao de Matrizes (SELECAO)
+* @ Data: 02.05.2018
+* @ Autor: Eduardo Almeida
+* @ Matricula: 17.2.000024
+*
+*/
+
+int main()
+{
+	int vetor[TM][TM];
+	
+	int i,j, c = 0, aux = 0, k = 0, min = 0, troca = 0, cont = 0;
+	int size = sizeof(vetor) / 4;
+	
+	printf("\n");
+	for(i=0;i<TM;i++) {
+		for(j=0;j<TM;j++) {
+			vetor[i][j] = rand()%1000;
+		}
+		
+	} 
+	
+	printf("Iniciar... Enter!");
+	printf("\n");
+	for (i=0;i<9;i++) 
+	{
+		imenor=i;
+		menor=m[i] [col];
+		for (k=i+1;k<10;k++)
+		{
+			if (m[k][col] < menor)
+			{
+				imenor=k; 
+				menor=m[k][col];
+			};
+		};
+ 		for (j=0;j<10;j++){ 
+ 		
+		 	aux=m[imenor][j]; 
+			m[imenor][j]=m[i][j];
+			m[i][j]=aux;
+			
+		 }			
+	};
+	
+	getchar();
+
+
+	for(i=0;i<TM;i++) {
+		for(j=0;j<TM;j++) {
+			printf("%5d", vetor[i][j]);
+		}
+		printf("\n");
+		
+	} 
+	
+
+
+}
